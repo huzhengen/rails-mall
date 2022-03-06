ded3dae7dbcebb2a4535ed0883b116f61d0c8845
huzhengen <huzhengen@163.com>
2022-03-06 18:17:38 +0800

    地址类型的类型

--- db/migrate/20220303063501_create_addresses.rb
+++ db/migrate/20220303063501_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
       t.integer :user_id
-      t.integer :address_type
+      t.string :address_type
       t.string :address
       t.string :contact_name
       t.string :cellphone
